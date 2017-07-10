15fe0c4240f8ecb7957e1d01a7bede8c7523434f
Mojganx <mojgan.zolghadr@gmail.com>
2017-07-10 21:17:12 +0200

    create show and new song page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,27 @@ class SongsController < ApplicationController
 
   def index
     @songs = Song.all
-  end 
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    song_params = params.require(:song).permit(:name)
+
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to @song
+    else
+      render 'new'
+    end
+  end
+
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -5,4 +5,5 @@ class Song < ApplicationRecord
   end
 
 
+
 end
