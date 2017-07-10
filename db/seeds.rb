fe00c769b572b198b983b4188b734d2cc9964ee4
Mojganx <mojgan.zolghadr@gmail.com>
2017-07-10 20:11:28 +0200

    Create song model

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+song1 = Song.create!({name: "Uprising"})
+song2 = Song.create!({name: "Seeing Red"})
+song3 = Song.create!({name: "Make a hundred"})
+song4 = Song.create!({name: "I Wanna Dance with Somebody"})
+song5 = Song.create!({name: "How Will I know"})
+song6 = Song.create!({name: "My Love is Your Love"})
+song7 = Song.create!({name: "The Trooper"})
+song8 = Song.create!({name: "Run to the Hills"})
+song9 = Song.create!({name: "Fear of the Dark"})
