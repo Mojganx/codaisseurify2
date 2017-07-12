4d9cd01147ca0d3040c0ce9465448187500ce6c7
Mojganx <mojgan.zolghadr@gmail.com>
2017-07-12 19:17:57 +0200

    one-to-many association tests completed

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Artist, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:name) }
   end
+
+  describe "association with songs" do
+     it { is_expected.to have_many :songs}
+  end
+
 end
