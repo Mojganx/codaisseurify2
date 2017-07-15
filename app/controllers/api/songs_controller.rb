de94f31f1d228ba65b0011739c6264815205948c
Mojganx <mojgan.zolghadr@gmail.com>
2017-07-15 15:42:25 +0200

    Add API songs destroy

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -29,6 +29,17 @@ class Api::SongsController < ApplicationController
      end
    end
 
+
+  def destroy
+    song = Song.find(params[:id])
+    song.destroy
+
+    render status: 200, json: {
+      message: "Song deleted"
+    }.to_json
+  end
+
+
    private
 
    def song_params
