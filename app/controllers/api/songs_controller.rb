35da0d1e107c187d120827e56d77709334eb5793
Mojganx <mojgan.zolghadr@gmail.com>
2017-07-15 15:37:42 +0200

    Add API songs show and create

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,7 +1,39 @@
 class Api::SongsController < ApplicationController
+
   def index
     render status: 200, json: {
       songs: Song.all
     }.to_json
   end
+
+  def show
+   song = Song.find(params[:id])
+
+   render status: 200, json: {
+     song: song
+   }.to_json
+ end
+
+   def create
+     song = Song.new(song_params)
+
+     if song.save
+       render status: 201, json: {
+         message: "Song created",
+         song: song
+       }.to_json
+     else
+       render status: 422, json: {
+         errors: song.errors
+       }.to_json
+     end
+   end
+
+   private
+
+   def song_params
+     params.require(:song).permit(:name, :artist_id)
+   end
+
+
 end
